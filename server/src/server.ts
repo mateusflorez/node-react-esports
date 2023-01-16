40bd461f58bdd836c7760899f4b50d16971a2c17
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-01-16 11:02:25 -0300

    create ad route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
+import { convertHourStringToMinute } from './utils/convert-hour-string-to-minutes'
 
 const app = express()
 const prisma = new PrismaClient({
     log: ['query']
 })
+
+app.use(express.json())
 
 app.get('/games', async (req, res) => {
     const games = await prisma.game.findMany({
@@ -41,7 +44,27 @@ app.get('/games/:id/ads', async (req, res) => {
         }
     })
 
-    return res.json([ads])
+    return res.json(ads)
+})
+
+app.post('/games/:id/ads', async (req, res) => {
+    const gameId = req.params.id
+    const body = req.body
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays,
+            hourStart: convertHourStringToMinute(body.hourStart),
+            hourEnd: convertHourStringToMinute(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+
+    return res.status(201).json(ad)
 })
 
 app.get('/ads/:id/discord', async (req, res) => {
