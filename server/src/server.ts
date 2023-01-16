e72f99ddd3818239f6291cf8bd7741072ca4d254
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-01-16 09:53:17 -0300

    add prisma + model game

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,20 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+    return res.status(200).json([])
+})
+
+app.get('games/:id/ads', (req, res) => {
+    return res.json([
+        { 'id': 1, name: 'Ad 1' },
+        { 'id': 2, name: 'Ad 2' },
+        { 'id': 3, name: 'Ad 3' },
+        { 'id': 4, name: 'Ad 4' },
+    ])
+})
+
+app.get('ads/:id/discord', (req, res) => {
     return res.json([
         { 'id': 1, name: 'Ad 1' },
         { 'id': 2, name: 'Ad 2' },
